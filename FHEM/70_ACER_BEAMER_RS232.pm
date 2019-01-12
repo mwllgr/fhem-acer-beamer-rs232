c38fa5011a6835ae00d268f101163ad9924086d6
Zadolux (M.) <zadolux@inscomers.net>
2019-01-12 15:18:16 +0100

    Added gets, work but have a bug
    Have to determine when it's the end of the response (beamer sends \r after values)

--- FHEM/70_ACER_BEAMER_RS232.pm
+++ FHEM/70_ACER_BEAMER_RS232.pm
@@ -1,7 +1,7 @@
 ##############################################
 # $Id
 #
-# Created originally by Markus Bloch, edited by Zadolux for Acer beamers.
+# created originally by Markus Bloch, edited by Zadolux for Acer beamers
 #
 # This modules controls Acer beamers which are connected via RS232.
 #
@@ -9,8 +9,9 @@
 # http://www.hifi-forum.de/viewthread-94-10979.html
 #
 # Define:  define Beamer ACER_BEAMER_RS232 /dev/ttyUSB0
+#
 # Set: source:hmdi,dsub, ...
-# 
+#
 
 package main;
 
@@ -18,6 +19,13 @@ use strict;
 use warnings;
 use Time::HiRes qw(gettimeofday);
 use DevIo;
+
+my %ACER_BEAMER_RS232_get = (
+  "manufacturer" => "* 0 IR 037",
+  "model" => "* 0 IR 035",
+  "source" => "* 0 Src ?",
+  "lampHours" => "* 0 Lamp",
+);
 
 my %ACER_BEAMER_RS232_set = (
 "remoteControl" => {
@@ -56,6 +64,8 @@ my %ACER_BEAMER_RS232_set = (
            }
 );
 
+
+
 #####################################
 sub
 ACER_BEAMER_RS232_Initialize($)
@@ -64,7 +74,9 @@ ACER_BEAMER_RS232_Initialize($)
 
   $hash->{DefFn}    = "ACER_BEAMER_RS232_Define";
   $hash->{UndefFn}  = "ACER_BEAMER_RS232_Undef";
+  $hash->{ReadFn} = "ACER_BEAMER_RS232_Read";
   $hash->{SetFn}    = "ACER_BEAMER_RS232_Set";
+  $hash->{GetFn}    = "ACER_BEAMER_RS232_Get";
   $hash->{ReadyFn}  = "ACER_BEAMER_RS232_Ready";
   $hash->{AttrList} = " ".$readingFnAttributes;
 }
@@ -76,17 +88,19 @@ ACER_BEAMER_RS232_Define($$)
   my ($hash, $def) = @_;
   my @a = split("[ \t][ \t]*", $def);
 
+
+
   my $name = $a[0];
   my $dev = $a[2];
-  
+
+
+
   $hash->{helper}{RECEIVE_BUFFER} = "";
   $dev .= "\@9600" if(not $dev =~ m/\@\d+/);
   $hash->{DeviceName} = $dev;
-  
   DevIo_CloseDev($hash);
-  
+
   my $ret = DevIo_OpenDev($hash, 0, undef);
-  
   delete($hash->{PARTIAL});
   RemoveInternalTimer($hash);
   return undef;
@@ -97,8 +111,8 @@ sub
 ACER_BEAMER_RS232_Undef($$)
 {
   my ($hash, $arg) = @_;
-  
-  DevIo_CloseDev($hash); 
+
+  DevIo_CloseDev($hash);
   return undef;
 }
 
@@ -107,23 +121,99 @@ sub
 ACER_BEAMER_RS232_Set($@)
 {
     my ($hash, @a) = @_;
-
     my $what = $a[1];
-    my $usage = "Unknown argument $what, choose one of ";
-
-    foreach my $cmd (sort keys %ACER_BEAMER_RS232_set)
-    {
-       $usage .= " $cmd:".join(",", sort keys %{$ACER_BEAMER_RS232_set{$cmd}});
-    }
 
     if(exists($ACER_BEAMER_RS232_set{$what}) and exists($ACER_BEAMER_RS232_set{$what}{$a[2]}))
     {
-        DevIo_SimpleWrite($hash, $ACER_BEAMER_RS232_set{$what}{$a[2]}."\n", 0);
+      $hash->{buffer} = "";
+      $hash->{lastGet} = "";
+
+      DevIo_SimpleWrite($hash, $ACER_BEAMER_RS232_set{$what}{$a[2]}."\r", 0);
     }
     else
     {
+      my $usage = "unknown argument $what choose one of ";
+
+      foreach my $cmd (sort keys %ACER_BEAMER_RS232_set)
+      {
+         $usage .= " $cmd:".join(",", sort keys %{$ACER_BEAMER_RS232_set{$cmd}});
+      }
+
       return $usage;
     }
+}
+
+sub
+ACER_BEAMER_RS232_Get($@)
+{
+    my ($hash, @a) = @_;
+    return "get needs at least an argument" if ( @a < 2 );
+
+    my $getName = $a[1];
+    my $name = shift @a;
+    my $attr = shift @a;
+    my $serialCmd;
+
+    if(!$ACER_BEAMER_RS232_get{$attr})
+    {
+        my @cList = keys %ACER_BEAMER_RS232_get;
+        return "unknown argument $attr choose one of " . join(" ", @cList);
+    }
+    else
+    {
+      $serialCmd = $ACER_BEAMER_RS232_get{$attr};
+      DevIo_SimpleWrite($hash, $serialCmd . "\r", 0);
+    }
+
+    $hash->{lastGet} = $getName;
+    $hash->{buffer} = "";
+    return "Read with command \"" . $serialCmd . "\" started, watch readings.";
+}
+
+sub ACER_BEAMER_RS232_Read($)
+{
+    my ($hash) = @_;
+    my $name = $hash->{NAME};
+    my $finalValue;
+
+    # read from serial device
+    my $buf = DevIo_SimpleRead($hash);
+    return "" if ( !defined($buf) );
+
+    $hash->{buffer} .= $buf;
+    if(index($hash->{buffer}, "*000") != -1)
+    {
+      Log3 $name, 5, "$name: Command accepted";
+      $hash->{cmdAccepted} = "yes";
+
+      if($hash->{lastGet} ne "")
+      {
+        $finalValue = substr($hash->{buffer}, 5);
+
+            if($hash->{lastGet} eq "manufacturer")
+            {
+              $finalValue =~ s/Name //;
+            }
+            elsif($hash->{lastGet} eq "source")
+            {
+              $finalValue =~ s/Src //;
+            }
+            elsif($hash->{lastGet} eq "lampHours" && $finalValue ne "")
+            {
+              $finalValue = sprintf("%d", $finalValue);
+            }
+
+            readingsBeginUpdate($hash);
+            readingsBulkUpdate($hash, $hash->{lastGet}, $finalValue);
+            readingsEndUpdate($hash, 1);
+      }
+    }
+    else
+    {
+      $hash->{cmdAccepted} = "no";
+    }
+
+    Log3 $name, 5, "$name: read buffer content: " . $hash->{buffer};
 }
 
 #####################################
@@ -136,4 +226,3 @@ ACER_BEAMER_RS232_Ready($)
 }
 
 1;
-
